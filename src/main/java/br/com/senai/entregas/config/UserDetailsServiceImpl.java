b498ebd571cc4090c89923d1f0053c57e3e23bfd
Renata Santos <renatarhc@outlook.com>
2025-09-19 21:54:49 -0300

    Aula 19-09-25

--- src/main/java/br/com/senai/entregas/config/UserDetailsServiceImpl.java
+++ src/main/java/br/com/senai/entregas/config/UserDetailsServiceImpl.java
@@ -1,16 +1,19 @@
-package br.com.senai.entregas.service;
+package br.com.senai.entregas.config;
 
 import br.com.senai.entregas.repository.UsuarioRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UsuarioRepository usuarioRepository;
 
-    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
+    public UserDetailsServiceImpl(UsuarioRepository repo) {
+
+        this.usuarioRepository = repo;
     }
 
 
